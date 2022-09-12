80b1f09083ecd8f3c8e51fe9cecfbcce0bcd6cc3
SUMANTA5 <sumanta111195@gmail.com>
2022-09-12 14:17:12 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect,useState} from 'react'
-import NavBar from './components/NavBar';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-   <NavBar/>
+      <NavBar/>
+      <Home/>
     </div>
-  
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 function NavBar() {
+  const [nav, setNav] = useState(false);
+
   const links = [
     {
       id: 1,
@@ -39,15 +41,36 @@ function NavBar() {
           <li
             key={id}
             className="px-4 cursor-pointer capitalize font-medium
-text-gray-500 hover:scale-110 duration-200"
+         text-gray-500 hover:scale-110 duration-200"
           >
             {link}
           </li>
         ))}
       </ul>
-      <div className="cursor-pointer pr-4 z-10 text-gray-500">
-        <FaBars size={30}/>
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
+
+      {nav && (
+        <ul
+          className="flex flex-col justify-center
+        items-center absolute top-0 left-0 w-full
+        h-screen bg-gradient-to-b from-black
+      to-gray-800 text-gray-500"
+        >
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
