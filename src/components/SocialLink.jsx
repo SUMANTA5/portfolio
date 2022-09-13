8a538715af1269d981b41c2bd764d18a52d39e0f
SUMANTA5 <sumanta111195@gmail.com>
2022-09-13 18:13:20 +0530

    fix all is complete

--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -40,7 +40,7 @@ const SocialLink = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "https://drive.google.com/file/d/1Z9rlF5nuT_NqwKsPPbw6VnX2v_44yYG-/view?usp=sharing",
+      href: "https://drive.google.com/file/d/1LjO60hJc8WLRLLeRdBZuUOJEwksyCo__/view?usp=sharing",
       style: "rounded-br-md",
       download: true,
     },
