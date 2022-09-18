4090036aa16195004dcef7a6bf3faaab01ae7db1
SUMANTA KUMAR DAS <48908320+SUMANTA5@users.noreply.github.com>
2022-09-18 12:22:25 +0530

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
+//import { HiOutlineMail } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Footer = () => {
   const footerIcon = [
@@ -17,7 +18,7 @@ const Footer = () => {
       id: 2,
       child: (
         <>
-          <FaGithub size={17}/>
+          <FaGithub size={17} />
         </>
       ),
       href: "https://github.com/SUMANTA5",
@@ -37,9 +38,14 @@ const Footer = () => {
     <div>
       <footer class="text-gray-600 body-font bg-gray-800">
         <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-          <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+          <Link
+            to="home"
+            smooth
+            duration={500}
+            class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 cursor-pointer"
+          >
             <span class="ml-3 text-xl font-signature text-white ">Sumanta</span>
-          </a>
+          </Link>
           <p class="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
             2022
           </p>
@@ -48,6 +54,7 @@ const Footer = () => {
               <a
                 key={id}
                 href={href}
+                rel="noreferrer" 
                 target="_blank"
                 class="m-2 cursor-pointer text-gray-500 font  hover:text-white"
               >
